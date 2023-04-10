4548b776afb7c3fe78c10b303a0f3430605abff3
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2023-04-10 22:53:29 +0900

    Item쪽 코드 작성

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -1,28 +1,57 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest // 이 두가지가 있어야 ~~
+@Transactional // 롤백을 위해서
 public class MemberServiceTest {
 
-    @Test
-    public void 회원가입() throws Exception{
-        //given
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
 
+    
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setName("kim");
+        
         //when
+        Long savedId = memberService.join(member);
 
         //then
+        em.flush();
+        assertEquals(member, memberRepository.findOne(savedId));
     }
 
-    @Test
-    public void 중복_회원_예외() throws Exception{
+    @Test(expected = IllegalStateException.class)
+    public void 중복_회원_예외() throws Exception {
         //given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
+        memberService.join(member2);
 
         //then
+        fail("예외가 발생해야한다");
     }
-
-
 }
