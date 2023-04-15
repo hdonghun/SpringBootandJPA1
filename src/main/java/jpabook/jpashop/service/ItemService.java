90370757d168101581a336df634e14754c3322c6
Donghun HAN <67058000+hdonghun@users.noreply.github.com>
2023-04-15 18:31:42 +0900

    주문 취소 및 조회 기능 끝.

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +21,23 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    // 변경감지 사용
+    @Transactional // Transactional에 의해서, 커밋이 된다. 플러쉬를 날린다? 영속성 컨테스트에서 변경된것은 무엇인지 찾는다. 찾은후에 그것을 데이터 업데이트쿼리를 날려서 DB에 업데이트한다.
+    // 변경감지에 의해서 데이터 처리하는 방법임.
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId); // 여기서 findItem으로 가져온것은 영속상태이다.
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+    }
+
+    // 병합 사용하기
+    // 병합은 준영속 상태의 엔티티를 영속 상태로 변경할 떄 사용하는 기능이다.
+
+
     public List<Item> findItems(){
         return itemRepository.findAll();
     }
-
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -5,6 +5,7 @@ import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import jpabook.jpashop.repository.MemberRepository;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -45,6 +46,14 @@ public class OrderService {
 
     }
 
+    /*
+    검색
+     */
+    public List<Order> findOrders(OrderSearch orderSearch){
+        return orderRepository.findAllByString(orderSearch);
+    }
+
+
      /*
     주문 최소
      */
@@ -56,11 +65,6 @@ public class OrderService {
         order.cancel();
     }
 
-    /*
-    검색
-     */
-//    public List<Order> findOrders(OrderSearch orderSearch){
-//        return orderRepository.findAll(orderSearch);
-//    }
+
 
 }
